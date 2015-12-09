d544044e45212d367de6cfa54cb9816cc95d8ac8
Dmitriy Bielorusov <develby@gmail.com>
2015-12-09 15:12:55 +0200

    Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // npm init
 // npm install webpack express --save
-// npm install react react-dom --save
+// npm install react react-dom react-router history@1.13.1 --save
+
 // npm install babel-core babel-loader babel-preset-es2015 babel-preset-react  --save
 // npm install babel-cli react-hot-loader webpack-dev-middleware webpack-hot-middleware --save-dev
 
@@ -9,10 +10,29 @@
 // ./node_modules/babel-cli/bin/babel-node.js ./src/server.dev.js
 import React from 'react';
 import { render } from 'react-dom';
-import About from './components/About';
+
+import  {Router, Route, IndexRoute } from 'react-router';
+
+import createHistory from 'history/lib/createBrowserHistory';
+
+import App     from './components/App';
+import About   from './components/About';
 import Welcome from './components/Welcome';
 
+const history = createHistory('/');
 
-render(<Welcome />, document.getElementById('app'));
+const routes = (
+  <Route path='/'            component={App}>
+    <IndexRoute              component={Welcome}/>
+    <Route      path='about' component={About}/>
+  </Route>
+);
 
-render(<Goodbye />, document.getElementById('app2'));
+// render(<Welcome />, document.getElementById('app'));
+// render(<Goodbye />, document.getElementById('app2'));
+
+
+render(
+  <Router hisory={history}>{routes}</Router>,
+  document.getElementById('app')
+);
